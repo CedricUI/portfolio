f84c147182f7de58c2d2f4a154e9b15108d1272a
cedric.boucher1990@gmail.com <cedric.boucher1990@gmail.com>
2026-06-27 11:17:46 +0200

    feat(effects/): amélioration de l'effet pour les malvoyant

--- scripts/utils/closeButton.js
+++ scripts/utils/closeButton.js
@@ -1,19 +1,20 @@
 export function closeBtn(idBtn, idClose, effect){
     console.log("effect", effect)
     if (idClose === undefined || idClose === null && idClose === undefined || idClose === null) {
-        console.info(`Paramètre manquant:\n Nom de l'id du bouton : ${idClose} - Nom de l'id de l'élément manquant ${idClose}`)
+        console.error(`Paramètre manquant:\n Nom de l'id du bouton : ${idClose} - Nom de l'id de l'élément manquant ${idClose}`)
     }
     
     idBtn.addEventListener("click", () =>{
 
         if (typeof effect === "function"){
             effect(idClose)
+            idClose.style.transform = "translateY(1000000000px)"
         }else if (
             typeof effect === "number" ||
             typeof effect === "string" ||
             Array.isArray(effect)
         ){
-            console.info("Erreur de paramètre")
+            console.error(`Erreur de paramètre ; ${effect} n'est pas une fonction`)
         }else{
             idClose.style.display = "none"
         }
