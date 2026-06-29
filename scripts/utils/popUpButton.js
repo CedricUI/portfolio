409974d446d607b9e726126ab316b9e5c7a73158
cedric.boucher1990@gmail.com <cedric.boucher1990@gmail.com>
2026-06-29 14:11:01 +0200

    factory(cookie.js) : simplifier  pop-up.js

--- scripts/utils/popUpButton.js
+++ scripts/utils/popUpButton.js
@@ -1,34 +1,38 @@
 import { setCookie, getCookie } from './cookie.js'
 
 export async function popUpButton(idBtn, idClose, effect){
-    
-    if (idClose === undefined || idClose === null && idBtn === undefined || idBtn === null) {
+    const conditionIdBtn = typeof idBtn !== "object" && idBtn !== null && !Array.isArray(idBtn) && idBtn !== undefined 
+    const conditionIdClose = typeof idClose !== "object" && idClose !== null && !Array.isArray(idClose) && idClose !== undefined
+
+    if (conditionIdBtn || conditionIdClose) {
         console.error(`Paramètre manquant:\n Nom de l'id du bouton : ${idBtn} - Nom de l'id de l'élément manquant ${idClose}`)
-    }else if(await getCookie(idClose.id)) {
+    }
+    
+    if(await getCookie(idClose.id)) {
         idClose.style.display = "none"
         document.body.style.overflow = ""
     }else{
         let ok = true
         idBtn.addEventListener("click", () =>{
-            if (typeof effect === "number" || typeof effect === "string" || Array.isArray(effect)){
+
+            if (typeof effect !== "function"){
                 console.error(`Erreur de paramètre ; ${effect} n'est pas une fonction`)
                 ok = false
             }else if (typeof effect === "function"){
-    
                 effect(idClose)
     
                 const display = () =>{
                     idClose.style.display = "none"
                 }
-    
                 setTimeout(display, 5000);
+
+                document.body.style.overflow = ""
             }else{
                 idClose.style.display = "none"
+                document.body.style.overflow = ""
             }
     
-            document.body.style.overflow = ""
-            if (ok) { setCookie(idClose.id, "true", null)}
-
+            if (ok) setCookie(idClose.id, "true", null)
         })
     }
 }
