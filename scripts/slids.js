888c1ebf9f9f5312d92528f9ee59fbec0c4eee95
cedric.boucher1990@gmail.com <cedric.boucher1990@gmail.com>
2026-07-16 22:00:13 +0200

    factory: simplifier les boutons slids

--- scripts/slids.js
+++ scripts/slids.js
@@ -6,10 +6,11 @@ const flexSild = domElement.flexSild
 const btnProjects = domElement.btnProjects
 
 
+
 export const projects = [
-    {index: 0, link:"https://github.com/CedricUI/forum", title:"Forum", image:"asset/video/forum.gif", description:"Forum web développé en Go avec SQLite, intégrant authentification, posts, commentaires, likes/dislikes, filtres et conteneurisation Docker.", languages:["Go", "HTML", "CSS", "JS", "SQLite"]},
-    {index: 1, link:"https://github.com/CedricUI/lem-in", title:"Lem-in", image:"https://www.gifsanimes.com/data/media/183/fourmi-image-animee-0029.gif", description:"Simulation de colonie de fourmis en Go utilisant des algorithmes de recherche de chemin pour optimiser les déplacements dans un graphe.", languages:["Go"]},
-    {index: 2, link:"https://evreux-muay-thai-gym.fr/", title:"Site E-commerce WP", image:"asset/video/evreux-muya-thai.gif", description:"Site e-commerce WordPress dédié à la boxe thaï : achat de licences,Investigation, t-shirts et équipements sportifs en ligne.", languages:["WordPress"]}
+    { link:"https://github.com/CedricUI/forum", title:"Forum", image:"asset/video/forum.gif", description:"Forum web développé en Go avec SQLite, intégrant authentification, posts, commentaires, likes/dislikes, filtres et conteneurisation Docker.", languages:["Go", "HTML", "CSS", "JS", "SQLite"]},
+    { link:"https://github.com/CedricUI/lem-in", title:"Lem-in", image:"https://www.gifsanimes.com/data/media/183/fourmi-image-animee-0029.gif", description:"Simulation de colonie de fourmis en Go utilisant des algorithmes de recherche de chemin pour optimiser les déplacements dans un graphe.", languages:["Go"]},
+    { link:"https://evreux-muay-thai-gym.fr/", title:"Site E-commerce WP", image:"asset/video/evreux-muya-thai.gif", description:"Site e-commerce WordPress dédié à la boxe thaï : achat de licences,Investigation, t-shirts et équipements sportifs en ligne.", languages:["WordPress"]}
 ];
 slides[0].innerHTML = projects.map(project => `
     <div id = ${project.index}>
@@ -32,40 +33,30 @@ slides[0].innerHTML = projects.map(project => `
         </dd>
     </div>
 `).join('');
- const menu = document.createElement('menu')
+
+const menu = document.createElement('menu')
+
 for (let index = 0; index < projects.length ; index++) {
     const button = document.createElement('button')
     button.className ='btn-projects'
     button.append(projects[index].title)
     menu.append(button) 
 }
+
 flexSild.append(menu)
 
-if (window.innerWidth >= 768 && window.innerWidth <= 1100){
-    console.log("window.innerWidth", window.innerWidth)
+const SLIDE_STEP = 531;
 
-    btnProjects[0].addEventListener("click", () => {
-        slid[0].scrollTo({
-            left: 0,
+const attachSlideButton = (button, index, container) => {
+    button.addEventListener("click", () => {
+        container.scrollTo({
+            left: SLIDE_STEP * index,
             behavior: "smooth"
         });
-    })
-    
-    btnProjects[1].addEventListener("click", () => {
-        slid[0].scrollTo({
-            left: 531,
-            behavior: "smooth"
-        });
-    })
-    
-    btnProjects[2].addEventListener("click", () => {
-        slid[0].scrollTo({
-            left: 531*2,
-            behavior: "smooth"
-        });
-    })
-}
-
-
-
+    });
+};
+const btnProjectsArray = Array.from(btnProjects);
+btnProjectsArray.forEach((button, index) => {
+    attachSlideButton(button, index, slid[0]);
+});
             
